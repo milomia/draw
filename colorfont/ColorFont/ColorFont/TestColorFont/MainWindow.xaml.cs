ca1b5d2b54f1c8d8f6cd3e5cb4bf7a08e77869ec
milomia <m-hunt3@sky.com>
2017-02-25 17:54:04 +0000

    resync

---
+++ colorfont/ColorFont/ColorFont/TestColorFont/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using ColorFont;
+
+namespace TestColorFont
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ColorFont.ColorFontDialog fntDialog = new ColorFont.ColorFontDialog();
+            fntDialog.Owner = this;
+            fntDialog.Font = FontInfo.GetControlFont(this.txtText);
+            if (fntDialog.ShowDialog() == true)
+            {
+                FontInfo selectedFont = fntDialog.Font;
+                
+                if (selectedFont != null)
+                {
+                    FontInfo.ApplyFont(this.txtText, selectedFont);
+                }
+            }
+        }
+    }
+}
